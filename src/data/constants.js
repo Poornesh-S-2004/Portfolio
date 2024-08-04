9ba28d552ebb68181ca00a511ac6883310308eee
Poornesh.S <Poornesh.S>
2024-08-04 08:49:46 +0530

    Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,187 @@
+export const Bio = {
+  name: "Poornesh.S",
+  roles: [
+    "Front End Developer"
+  ],
+  description:
+    "A skilled React developer with a strong portfolio, passionate about staying updated on frontend tech. Collaborative and committed to quality, you thrive in dynamic teams. Eager to contribute creativity and bring innovative ideas to life.",
+  github: "https://github.com/Poornesh1234567/",
+  resume:
+    "https://drive.google.com/file/d/1D2SQYoqGx3b3mxqdc8pFNROvTuyoMgR0/view?usp=drive_link/",
+  linkedin: "https://www.linkedin.com/in/poornesh-sivakumar-a5327228b/",
+  email: "mailto: poornesheduction@gmail.com",
+  insta: "https://www.instagram.com/___poornesh___/",
+  phone: "tel:+91 9944941318",
+  whatsapp:9944941318
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ],
+  },
+  
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://seeklogo.com/images/G/git-logo-CD8D6F1C09-seeklogo.com.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          // "https://github.com/Poornesh1234567/",
+          "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    
+    role: "Personalisation",
+    abt: "A glimpse of my individuality",
+    
+    desc: "As a budding React virtuoso , I'm fervently dedicated to crafting web experiences that transcend the ordinary, infusing every pixel with elegance and enchantment . My unwavering passion for unraveling intricate challenges fuels my odyssey in the realm of front-end wizardry . The prospect of harmonizing with a dynamic ensemble , while imbibing wisdom from industry sages , thrills me as I amplify my proficiencies in this ever-mutating domain.",
+   
+  },
+  {
+    id: 1,
+    
+    role: "Reason to hire me",
+    abt: "What Sets Me Apart.",
+    desc: "Aspiring React Frontend Developer passionate about crafting exceptional user experiences. I bring fresh perspectives, boundless ambition, and unconventional thinking to every project. My goal is to create digital magic through energetic, innovative work. Let's connect and embark on a journey to bring unique, ambitious, and engaging experiences to life.",
+  
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 1,
+    
+    school: "K.G College of Arts and Science",
+    date: "2022-present",
+    grade: "Upto - 4th Sem : 85.5% ",
+    desc: "During college, I learned web programming through hands-on projects and coursework. I gained proficiency in HTML, CSS, JavaScript, and various frameworks, which helped me build dynamic, responsive websites. My education covered both front-end and back-end development, enhancing my ability to create and maintain comprehensive web applications.",
+    degree: "Bachelor of Computer Applications",
+  },
+  {
+    id: 2,
+    
+    school: "Keartiman Matric and Higher Secondary School",
+    date: "2021-2022",
+    grade: "84%",
+    desc: "I've embraced a dynamic academic journey. My studies have fostered critical thinking and a thirst for knowledge. Through my dedication and adaptability, I've honed essential skills that transcend disciplines, preparing me for future challenges and opportunities.",
+    degree: "HSC- XII(std) (Arts Computer Applications)",
+  },
+  {
+    id: 3,
+    
+    school: "Keartiman Matric and Higher Secondary School",
+    date: "2019-2020",
+    grade: "55.6%",
+    desc: "I embarked on my educational journey with enthusiasm. This pivotal year instilled a strong foundation in essential subjects and time management. It was a crucial step toward my academic and personal development, preparing me for the road ahead",
+    degree: "SSLC - Xth(std)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Restoron",
+    date: "Since : 2004",
+    description:
+      "Experience unparalleled comfort and elegance at our premier hotel, featuring luxurious accommodations, exceptional service, and world-class amenities for discerning travelers.",
+    image:
+      "https://media.istockphoto.com/id/1446478827/photo/a-chef-is-cooking-in-his-restaurants-kitchen.jpg?s=612x612&w=0&k=20&c=jwKJmGErrLe2XsTWNYEEyiNicudYVA4j8jvnTiJdp58=",
+    tags: [
+
+     "HTML",
+     "CSS",
+     "JavaScript"
+     
+    ],
+    category: "landing pages",
+    github: "https://github.com/Poornesh1234567/Restoron",
+    webapp: "https://main--roaring-horse-e38d13.netlify.app/",
+  },
+ 
+ 
+  // {
+  //   id: 1,
+  //   title: "Movix - Immerse Yourself in Cinematic Bliss!",
+  //   date: "2024",
+  //   description:
+  //   "Welcome to Movix, where the magic of movies comes to life on your screen! As a movie viewing app designed for enthusiasts like you, Movix brings an unparalleled cinematic experience to your fingertips. Explore a vast library of films spanning genres from action and adventure to drama and romance. With user-friendly navigation and high-quality streaming, Movix ensures that every movie night is a journey into captivating storytelling and stunning visuals. Elevate your movie-watching experience with Movix - Your Gateway to Cinematic Excellence!",
+  //   image:
+  //     "https://github.com/navanee1609/movix/assets/120004894/10b4f51b-62b4-49d2-aa62-a270e98c24f6",
+  //   tags: [
+  //     "React","React-Redux", "TMDB API - Axios"
+  //   ],
+  //   category: "website",
+  //   github: "https://github.com/navanee1609/movix",
+  //   webapp: "https://movix-watch.netlify.app/",
+  // },
+
+  // {
+  //   id: 1,
+  //   title: "Retrotift - Where Gaming Nostalgia Meets Modern Excitement!",
+  //   date: "2024",
+  //   description:
+  //   "RetroRift, showcased in my portfolio, is a dynamic gaming platform uniting a diverse game library, global community engagement, and exclusive content. Seamlessly blending nostalgia with modern gaming trends, RetroRift offers a user-friendly interface and hosts live events. Explore the convergence of gaming culture at RetroRift—your ultimate gaming destination",
+  //   image:
+  //     "https://github.com/navanee1609/RETRORIFT/assets/120004894/998426a6-233b-4b78-becd-161d1410a320",
+  //   tags: [
+  //     "React","React-Redux", "RAWG.API - Axios"
+  //   ],
+  //   category: "website",
+  //   github: "https://github.com/navanee1609/RETRORIFT",
+  //   webapp: "https://retrorift-gaming-paradise.netlify.app/",
+  // },
+
+];
+
+export const TimeLineData = [
+  { year: 2015, text: "Completed SSLC" },
+  { year: 2017, text: "Completed HSC" },
+  { year: 2018, text: "Joined College" },
+  { year: 2022, text: "Completed my Bachelor's" },
+  { year: 2021, text: "Started my career as Front-End Developer" },
+];
